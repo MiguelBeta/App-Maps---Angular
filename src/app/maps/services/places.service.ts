9d8f135b490d5511893086d0323334b806c72b0a
Miguel <workingbeta01@gmail.com>
2024-03-02 15:13:36 -0500

    Diseño componentes, busquedas, detalles y peticion http

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +9,14 @@ export class PlacesService {
 
   public useLocation?: [ number, number ];
 
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
+
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor() {
+  constructor( private http: HttpClient ) {
     this.getUserLocation();
   }
 
@@ -33,6 +38,23 @@ export class PlacesService {
 
     });
 
+  }
+
+
+  getPlacesByQuery( query: string = '' ){
+    // todo: evaluar cuando el query es nulo
+
+    this.isLoadingPlaces = true;
+
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${ query }.json?limit=5&proximity=-73.98853656442466%2C40.728636535880696&language=es&access_token=pk.eyJ1IjoibWlndWVsYmV0YTEiLCJhIjoiY2x0N2MxdHg4MGs3bzJrcHRoaWloaXBlbyJ9.X3yyu94-685Mo97LQoELvw`)
+      .subscribe( resp => {
+
+        console.log( resp.features )
+
+        this.isLoadingPlaces = false;
+        this.places = resp.features
+      });
   }
 
 }
